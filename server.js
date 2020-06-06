cc65a63ca80746298bf191b37adb1f4492a75ee0
Karelle Hofler <karellehofler@gmail.com>
2020-06-05 20:48:35 -0400

    Frontend and backend deployed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const items = require('./routes/api/items');
+const path = require('path');
 
 const app = express();
 
@@ -17,6 +18,14 @@ mongoose.connect(db,{ useNewUrlParser: true })
 
 app.use('/api/items', items);
 
+//Server static assets if in production
+if(process.env.NODE_ENV) {
+    //Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
