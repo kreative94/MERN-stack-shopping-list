5c128e2d74bf0d97ddc2fcd5fb6d3de5391af918
Karelle Hofler <karellehofler@gmail.com>
2020-07-23 11:18:23 -0400

    v1.2

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { devToolsEnhancer, composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk';
 // import ReduxThunk from 'redux-thunk';
 import rootReducer from './reducers';
@@ -7,11 +8,19 @@ const initialState = {};
 
 const middleWare = [thunk];
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//In Development
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(
+//     rootReducer, initialState, composeEnhancers(
+//         applyMiddleware(...middleWare)
+//             ));
 
-const store = createStore(
-    rootReducer, initialState, composeEnhancers(
-        applyMiddleware(...middleWare)
-            ));
+const composeEnhancers = composeWithDevTools({
+
+})
+
+const store = createStore(rootReducer, initialState, composeEnhancers(
+    applyMiddleware(...middleWare)
+));
 
 export default store;
