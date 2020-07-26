e72206bbd844fdbd262162887b25454785d7687e
Karelle Hofler <karellehofler@gmail.com>
2020-07-26 18:14:21 -0400

    more changes

--- server.js
+++ server.js
@@ -11,19 +11,19 @@ const authRoute = require('./routes/api/auth');
 const listsRoute = require('./routes/api/lists');
 const usersRoute = require('./routes/api/users');
 
-const { MONGO_URI, PORT } = config;
+const { MONGO_URI, MONGO_DB_NAME, PORT } = config;
 
+// const { MONGO_URI } = "mongodb+srv://khofler:Sekirei11%21@cluster0-nnhyq.mongodb.net/list?retryWrites=true&w=majority"
 const app = express();
-
 //Body Parser Middleware
 app.use(cors());
 // app.use(express.json());
 app.use(bodyParser.json());
-
 //Db config
 // const db = config.get('mongoURI');
 // const db = require('./config/database').mongoURI;
-const db = `${MONGO_URI}`;
+// const db = `${MONGO_URI}`;
+const db = `${MONGO_URI}/${MONGO_DB_NAME}?retryWrites=true&w=majority`;
 
 mongoose.connect(db, { 
     useNewUrlParser: true,
